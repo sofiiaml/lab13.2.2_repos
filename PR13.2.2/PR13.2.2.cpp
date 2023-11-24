6b068435afbed278c079ee1d852175d60dfe6ea9
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-11-24 11:47:38 +0200

    13.2.2.

---
+++ PR13.2.2/PR13.2.2.cpp
@@ -0,0 +1,47 @@
+﻿#include<string>
+#include <iostream>
+#include<fstream>
+
+#define CHECK_LETTER(x) (x>=65&&x<=90||x>=97&&x<=122)?(true):(false)
+#define CHECK_NUM(x) (x>=48&&x<=57)?(true):(false)
+#define CHECK_PUNCT(x)(x==46||x==44)?(true):(false)
+#define PRINT(n,l,p) cout << n << " " << l << " " << p << " " << n << " " << l << endl
+#define PRINT_TYPE(t) cout<<t<<" type that is not present"<<endl
+using namespace std;
+
+
+int main()
+{
+    string line;
+    ifstream file("Data.txt");
+    char num=' ', letter=' ', punct=' ';
+    if (file.is_open()) {
+        getline(file, line);
+        for (int i = 0; i < line.size(); i++) {
+            if (CHECK_LETTER(line[i])) {
+                letter = line[i];
+            }
+            if (CHECK_NUM(line[i])) {
+                num = line[i];
+            }
+            if (CHECK_PUNCT(line[i])) {
+                punct = line[i];
+            }
+        }
+        PRINT(num, letter, punct);
+        if (num == ' ') {
+            PRINT_TYPE("number");
+        }
+        if (letter == ' ') {
+            PRINT_TYPE("letter");
+        }
+        if (punct == ' ') {
+            PRINT_TYPE("punct");
+        }
+    }
+    else {
+        cout << "Error file is not opened" << endl;
+    }
+}
+
+
---
+++ UnitTest13.2.2/UnitTest13.2.2.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR13.2.2/PR13.2.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1322
+{
+	TEST_CLASS(UnitTest1322)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char num = '5', letter = 'k', punct = ',';
+			Assert::IsTrue(CHECK_NUM(num));
+			Assert::IsTrue(CHECK_LETTER(letter));
+			Assert::IsTrue(CHECK_PUNCT(punct));
+			Assert::IsFalse(CHECK_NUM(letter));
+			Assert::IsFalse(CHECK_NUM(punct));
+			Assert::IsFalse(CHECK_LETTER(punct));
+			Assert::IsFalse(CHECK_LETTER(num));
+			Assert::IsFalse(CHECK_PUNCT(letter));
+			Assert::IsFalse(CHECK_PUNCT(num));
+		}
+	};
+}
